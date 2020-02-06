42de69a2b9542c5fcae5ac400341aaceced5f9c6
LittleKitacho <little.kitacho@outlook.com>
2020-02-05 20:57:53 -0500

    Start work on a keyboard combo tester

--- stress_test.py
+++ stress_test.py
@@ -3,6 +3,12 @@ import datetime
 cpass = open('cpass.txt',"r").readlines()
 wrd_dict = open('dict.txt',"r").readlines()
 
+chars = "abcdefghijklmnopqrstuvwxyz"
+capt = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+kbd = ["`1234567890-=","qwertyuiop[]\\","asdfghjkl;'","zxcvbnm,./"," "]
+sft = ["~!@#$%^&*()_+","QWERTYUIOP{}|","ASDFGHJKL:\"","ZXCVBNM<>?"," "]
+
 def test(password: str) -> int:
     for i in cpass:
         if password == i.strip():
@@ -20,6 +26,26 @@ def test(password: str) -> int:
             strength -= 1
 
         x += 1
+
+    x = 0
+
+    while x < len(password):
+
+        sft = None
+        kbd_index = None
+        row_index = None
+
+        for row in kbd:
+            for char in row:
+                if l == password[x]:
+                    kbd_index = kbd.index(i)
+                    row_index = l.index(password[x])
+                    sft = False
+
+        if kbd_index == None:
+            for i in sft:
+                for l in i:
+                    if l == password[x]
 
     return strength
 
