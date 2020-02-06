50b6b97a1353a963e90bc87e0748ba43ceee3280
LittleKitacho <40306561+LittleKitacho@users.noreply.github.com>
2020-02-06 01:40:02 +0000

    Start work on a stress test.

--- stress_test.py
+++ stress_test.py
@@ -1,18 +1,29 @@
 import datetime
 
-chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890~`!@#$%^&*()-_=+[{]}\\|;:'\",<.>/? "
+cpass = open('cpass.txt',"r").readlines()
+wrd_dict = open('dict.txt',"r").readlines()
 
-def test (password: str) -> int:
-    x = len(password) - 1
-    strength = 0
+def test(password: str) -> int:
+    for i in cpass:
+        if password == i.strip():
+            return 0
+    
+    for i in wrd_dict:
+        if password == i.strip():
+            return 0
 
-    while x >= 0:
-        diff = x - len(password)
-        strength += chars.index(password[x]) * (10 ^ diff)
-        x -= 1
+    strength = len(password)
+    x = 0
 
-    return strength * -1
+    while x < len(password) - 1:
+        if password[x] == password[x + 1]:
+            strength -= 1
 
+        x += 1
+
+    return strength
 
 if __name__ == "__main__":
-    print("Password strength: " + str(test(input('Please give me a password to stress test:  '))))
+    passcode = input('Please give me a passcode to stress-test:  ')
+    pass_str = test(passcode)
+    print("Your password has a strength of " + str(pass_str))
